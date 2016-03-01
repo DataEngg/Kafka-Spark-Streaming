88e144e164337ea75e67f2306efe04d4e5d11c4e
Kunal <kunal.gupta@cube26.com>
2016-03-01 11:42:59 +0530

    Changes for making code generic

--- kafka-stream/kafka_stream.py
+++ kafka-stream/kafka_stream.py
@@ -4,7 +4,7 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-from properties import BROKER, TOPIC, ACTIONID, DURATION, APPNAME, MASTER
+from properties import BROKER, TOPIC, DURATION, APPNAME, MASTER, FILE
 
 
 class KafkaStream(object):
@@ -17,6 +17,8 @@ class KafkaStream(object):
         rdd_string_json = str(rdd[1])
         if "actionId" in rdd_string_json:
             json_values = json.loads(rdd_string_json)
+            json_match_value = KafkaStream.get_data()
+            ACTIONID = json_match_value["ACTIONID"]
             action_id = json_values.get("actionId")
             if action_id == ACTIONID:
                 return True
@@ -32,6 +34,14 @@ class KafkaStream(object):
         filter_values.pprint()
         stream_context.start()
         stream_context.awaitTermination()
+
+    @classmethod
+    def get_data(cls):
+        json_match_value = {}
+        with open(FILE, 'r') as file:
+            json_data = json.load(file)
+            json_match_value["ACTIONID"]=json_data.get("ACTIONID")
+        return json_match_value
 
 
 if __name__ == '__main__':
--- kafka-stream/properties.py
+++ kafka-stream/properties.py
@@ -1,6 +1,6 @@
 BROKER = "localhost:9092"
 TOPIC = "test"
-ACTIONID = "sessionLength"
 DURATION = 20
 APPNAME = "PythonKafkaStreaming"
 MASTER = "spark://Kunals-MBP.example.com:7077"
+FILE = "/Users/kunalgupta/Projects/KafkaSparkStreaming/kafka-stream/retrieve_data.json"
